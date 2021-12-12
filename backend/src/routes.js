354541d8b44d9cdb9b9adf0a81338ca869f4ef68
jfernandesdev <jfernandes.dev@gmail.com>
2021-12-12 10:51:34 -0300

    multer setup to upload locally

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,7 +1,11 @@
 const routes = require('express').Router();
+const multer = require('multer');
+const multerConfig = require('./config/multer');
 
-routes.get('/', (req, res) => {
-  return res.json({ hello: 'Hello World!' });
+routes.post('/posts', multer(multerConfig).single('file'), (req, res) => {
+  console.log(req.file);
+  
+  return res.json({ message: 'Imagem salva localmente!' });
 });
 
 module.exports = routes;
