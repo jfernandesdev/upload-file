6ed79b2ecf3443f0a51aebcd2a6bc4b0387b1996
jfernandesdev <jfernandes.dev@gmail.com>
2021-12-12 19:05:10 -0300

    route to list uploaded files

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -5,6 +5,7 @@ const multerS3 = require('multer-s3');
 
 const Post = require('./models/Post');
 
+// route to upload new files
 routes.post('/posts', multer(multerConfig).single('file'), async (req, res) => {
   const { originalname: name, size, key, location: url = '' } = req.file;
 
@@ -16,6 +17,13 @@ routes.post('/posts', multer(multerConfig).single('file'), async (req, res) => {
   });
   
   return res.json(post);
+});
+
+// route to list uploaded files
+routes.get('/posts', async(req, res) => {
+  const posts = await Post.find();
+
+  return res.json(posts);
 });
 
 module.exports = routes;
